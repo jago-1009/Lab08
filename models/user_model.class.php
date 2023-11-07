9726482036c16f85c7810575ed919b5ae1be1326
jago-1009 <garwoodj22@gmail.com>
2023-11-07 09:30:50 -0500

    Update user_model.class.php

--- models/user_model.class.php
+++ models/user_model.class.php
@@ -4,9 +4,28 @@
  * Author: Jacob Garwood
  * Date: 11/7/2023
  * File: user_model.class.php
- * Description:
+ * Description: User Model Class for connecting to SQL server
  */
 class UserModel
 {
+//adds user into the user database
+public function add_user($username,$password,$email,$firstname,$lastname) {
+
+    $passwordSQL = password_hash($password);
+
+}
+
+//verifies user against the database
+public function verify_user($username,$password) {
+
+}
+//Logs out by destroying the temporary cooking
+public function logout() {
+
+}
+//password reset form
+public function reset_password($username,$password) {
+
+}
 
 }
