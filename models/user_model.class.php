852b363f3ee4d37ec0c6b3111a323caa879b6cd8
jago-1009 <garwoodj22@gmail.com>
2023-11-07 12:12:52 -0500

    Update user_model.class.php

--- models/user_model.class.php
+++ models/user_model.class.php
@@ -8,18 +8,37 @@
  */
 class UserModel
 {
+    //declare SQL Parameters
+    private $db;
+    private $dbConnection;
+    static private $_instance = NULL;
+    private $tblUsers;
 //adds user into the user database
 public function add_user($username,$password,$email,$firstname,$lastname) {
+    $username = FILTER_SANITIZE_STRING($username);
+    $email = FILTER_SANITIZE_STRING($email);
+    $fName = FILTER_SANITIZE_STRING($firstname);
+    $lName = FILTER_SANITIZE_STRING($lastname);
+    $password = password_hash($password);
+    $sql = `INSERT INTO $this->tblUsers ($username,$password,$email,$fName,$lName)`;
+    $query = $this->dbConnection->query($sql);
 
-    $passwordSQL = password_hash($password);
 
 }
 
 //verifies user against the database
 public function verify_user($username,$password) {
 
+    if ($password == password_verify($passwordSQL) && $username == $usernameSQL) {
+    setcookie($username,$password);
+    return true;
+    }
+    else  {
+        return false;
+    }
+
 }
-//Logs out by destroying the temporary cooking
+//Logs out by destroying the temporary cookie
 public function logout() {
 
 }
