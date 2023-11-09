ba810891cb7110e9909dde16291ac21d64373e98
deep20-ig <78451396+deep20-ig@users.noreply.github.com>
2023-11-08 19:18:47 -0500

    sdas

---
+++ controllers/user_controller.class.php
@@ -0,0 +1,82 @@
+<?php
+/*
+* Author: Deep Patel
+* Date: 11/8/23
+* File: user_controller.class.php
+* Description: this is the user controller class
+*/
+
+class UserController{
+
+    private $user_model;
+
+    public function __construct(){
+        //creating an instance of the UserModel class
+        $this->user_model = new UserModel();
+    }
+
+    //index that stores all user information
+    public function  index(){
+        //retrieve from users
+       $view = new Index();
+       $view->display();
+    }
+
+    //create a user account by calling the addUser method of a userModel object
+    public function register() {
+        //call the addUser method of the UserModel object
+        $result = $this->user_model->add_user();
+
+        //display result
+        $view = new Register();
+        $view->display($result);
+    }
+
+    //display the login form
+    public function login() {
+        $view = new Login();
+        $view->display();
+    }
+
+    //verify username and password by calling the verifyUser method
+    public function verify() {
+        $result = $this->user_model->verify_user();
+        $view = new Verify();
+        $view->display($result);
+    }
+
+    //log out a user by using logout method
+    public function logout() {
+        $this->user_model->logout();
+        $view = new Logout();
+        $view->display();
+    }
+
+    //display the password reset form or an error message.
+    public function reset() {
+        if (!isset($_COOKIE['user'])) {  //if the user has not logged in
+            $this->error("To reset your password, please log in first.");
+        } else { //if the user has logged in.
+            $user = $_COOKIE['user'];
+            $view = new Reset();
+            $view->display($user);
+        }
+    }
+
+    //reset password by calling the resetPassword method in user model
+    public function do_reset() {
+
+        $result = $this->user_model->reset_password();
+        //exit($result);
+
+        $view = new ResetConfirm();
+        $view->display($result);
+    }
+
+    //display an error message
+    public function error($message) {
+        $view = new UserError();
+        $view->display($message);
+    }
+
+}
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * Author: your name
- * Date: today's date
+ * Author: Deep Patel
+ * Date: 11/7/23
  * Name: index.php
- * Description: short description about this file
+ * Description: The index for the users.
  */
 
 //include code in vendor/autoload.php file
@@ -13,4 +13,37 @@ require_once ("vendor/autoload.php");
 //create an object of UserController
 $user_controller = new UserController();
 
-//add your code below this line to complete this file
+//default action for users
+$action = "all";
+
+//retrieve the action from a querystring variable named "action"
+if (!($action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING))) {
+    $action = "index";  //default action
+}
+
+//take different action depending on the action value
+switch ($action) {
+    case "index":
+        $user_controller->index();
+        break;
+    case "register":
+        $user_controller->register();
+        break;
+    case "login":
+        $user_controller->login();
+        break;
+    case "verify":
+        $user_controller->verify();
+        break;
+    case "logout":
+        $user_controller->logout();
+        break;
+    case "reset":
+        $user_controller->reset();
+        break;
+    case "do_reset":
+        $user_controller->do_reset();
+        break;
+    default:
+        $user_controller->error("Invalid action was requested.");
+}
